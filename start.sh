3dd427940273ebaffb29aaedbb5b89edfbe72067
Ibrahim Hammad <62240944+ibrahim-egy@users.noreply.github.com>
2025-04-26 18:00:30 +0300

    updated start.sh req.txt

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-export PYTHONPATH=./src
-cd src
-gunicorn main:app -w 1 --log-file -
+## Ensure we are using pip3 to install the dependencies
+#echo "Installing dependencies from requirements.txt..."
+#pip3 install --no-cache-dir -r requirements.txt
+
+echo "Starting Flask app with Gunicorn..."
+export PYTHONPATH=.
+python3 -m gunicorn src.main:app -w 1 --bind 0.0.0.0:3000 --log-file -
+pp -w 1 --bind 0.0.0.0:3000 --log-file -
